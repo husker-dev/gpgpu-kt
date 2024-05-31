7e31b6082d62bb95e46db6daa954f2f6381556dc
Штенгауэр Никита Дмитриевич <nshtengauer@corp.finam.ru>
2024-05-31 11:23:15 +0300

    Changes in main class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
 package org.example
 
-import org.example.com.huskerdev.ta.engine.cpu.EngineCPUImpl
 import org.example.com.huskerdev.ta.engine.gpu.EngineGPUImpl
 import org.example.com.huskerdev.ta.sequence.*
 
 
 fun main() {
+    // Uncomment to use CPU (by default is GPU)
     //defaultEngine = EngineCPUImpl
+
     EngineGPUImpl.opencl    // Pre-initialize opencl
 
     // Arrays [0, 1, ..., n]
@@ -18,13 +19,19 @@ fun main() {
     val mult = s1 * s2 * s3
     val smth = sum + mult * 1000 / sqrt(s1) + pow(s2, 2)
 
-
-    val time = System.nanoTime()
-    val values = smth.get()
-    println("==========================")
-    println("completed in: ${(System.nanoTime() - time) / 1000000 / 1000.0} sec")
+    val values = speedometer { smth.get() }
 
     println("size: ${values.size} ${values.toShortString()}")
+}
+
+
+
+inline fun <T> speedometer(run: () -> T): T{
+    val time = System.nanoTime()
+    val result = run()
+    println("==========================")
+    println("completed in: ${(System.nanoTime() - time) / 1000000 / 1000.0} sec")
+    return result
 }
 
 fun FloatArray.toShortString(elements: Int = 6) =
